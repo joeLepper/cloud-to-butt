f2222f452c0fcfa4d97c8ab30d05f5a61e729594
Joe Lepper <josephlepper@gmail.com>
2018-09-04 14:22:14 -0700

    of questionable purpose.

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,4 +1,4 @@
-walk(document.body);
+if (window.location.host === 'developer.mozilla.org') walk(document.body);
 
 function walk(node) 
 {
@@ -36,10 +36,7 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\b?\b/g, "¯\_(ツ)_/¯");
 	
 	textNode.nodeValue = v;
 }
